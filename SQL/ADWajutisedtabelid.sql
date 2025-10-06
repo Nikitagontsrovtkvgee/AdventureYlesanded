60c6fdf6d8640e3eb0657bfb0460ef1b87f29b9c
Nikita Gontsarov <nikitosgoldbossgmail.com>
2025-10-06 11:06:27 +0300

    Trigerite, ja funktsioonide loomine

--- SQL/ADWajutisedtabelid.sql
+++ SQL/ADWajutisedtabelid.sql
@@ -1,1 +1,13 @@
- 
+CREATE TABLE #TempSalesSummary
+(
+    CustomerKey int,
+    TotalSales decimal(18,2),
+    AvgSales decimal(18,2)
+);
+
+INSERT INTO #TempSalesSummary(CustomerKey, TotalSales, AvgSales)
+SELECT CustomerKey, SUM(SalesAmount), AVG(SalesAmount)
+FROM FactInternetSales
+GROUP BY CustomerKey;
+
+SELECT * FROM #TempSalesSummary;
--- SQL/ADWfunktsioonid.sql
+++ SQL/ADWfunktsioonid.sql
@@ -1,1 +1,24 @@
- 
+CREATE FUNCTION dbo.SumSales(@ProductKey int)
+RETURNS decimal(18,2)
+AS
+BEGIN
+    DECLARE @TotalSales decimal(18,2)
+    SELECT @TotalSales = SUM(SalesAmount)
+    FROM FactInternetSales
+    WHERE ProductKey = @ProductKey
+    RETURN @TotalSales
+END;
+GO
+
+
+CREATE FUNCTION dbo.AvgSales(@ProductKey int)
+RETURNS decimal(18,2)
+AS
+BEGIN
+    DECLARE @AvgSales decimal(18,2)
+    SELECT @AvgSales = AVG(SalesAmount)
+    FROM FactInternetSales
+    WHERE ProductKey = @ProductKey
+    RETURN @AvgSales
+END;
+GO
