7afd4a2527bfcdae9a99741afc07edc4d55d4444
Opilane TTHK <opilane@tthk.ee>
2025-11-04 12:31:08 +0200

    5 suurima müügituluga toodet

--- SQL/ADWajutisedtabelid.sql
+++ SQL/ADWajutisedtabelid.sql
@@ -1,3 +1,8 @@
 SELECT ResellerKey, SUM(SalesAmount) AS TotalSales
 FROM FactResellerSales
 GROUP BY ResellerKey;
+
+SELECT TOP 5 ProductKey, SUM(SalesAmount) AS TotalSales
+FROM FactResellerSales
+GROUP BY ProductKey
+ORDER BY TotalSales DESC;
