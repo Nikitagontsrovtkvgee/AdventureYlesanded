0fd4cc89c65b9a5cc9574e8c927cdd55c68f36b7
Opilane TTHK <opilane@tthk.ee>
2025-11-04 12:29:22 +0200

    Kliendi müügitulu

--- SQL/ADWajutisedtabelid.sql
+++ SQL/ADWajutisedtabelid.sql
@@ -1,13 +1,3 @@
-CREATE TABLE #TempSalesSummary
-(
-    CustomerKey int,
-    TotalSales decimal(18,2),
-    AvgSales decimal(18,2)
-);
-
-INSERT INTO #TempSalesSummary(CustomerKey, TotalSales, AvgSales)
-SELECT CustomerKey, SUM(SalesAmount), AVG(SalesAmount)
-FROM FactInternetSales
-GROUP BY CustomerKey;
-
-SELECT * FROM #TempSalesSummary;
+SELECT ResellerKey, SUM(SalesAmount) AS TotalSales
+FROM FactResellerSales
+GROUP BY ResellerKey;
