1042afea6c3129dbc637f56147a0495a5ceaeaf2
Opilane TTHK <opilane@tthk.ee>
2025-11-04 12:33:33 +0200

    Müük kategooria järgi

--- SQL/ADWajutisedtabelid.sql
+++ SQL/ADWajutisedtabelid.sql
@@ -6,3 +6,11 @@ SELECT TOP 5 ProductKey, SUM(SalesAmount) AS TotalSales
 FROM FactResellerSales
 GROUP BY ProductKey
 ORDER BY TotalSales DESC;
+
+SELECT pc.ProductCategoryKey, pc.EnglishProductCategoryName, SUM(f.SalesAmount) AS TotalSales
+FROM FactResellerSales f
+JOIN DimProduct p ON f.ProductKey = p.ProductKey
+JOIN DimProductSubcategory sc ON p.ProductSubcategoryKey = sc.ProductSubcategoryKey
+JOIN DimProductCategory pc ON sc.ProductCategoryKey = pc.ProductCategoryKey
+GROUP BY pc.ProductCategoryKey, pc.EnglishProductCategoryName
+ORDER BY TotalSales DESC;
