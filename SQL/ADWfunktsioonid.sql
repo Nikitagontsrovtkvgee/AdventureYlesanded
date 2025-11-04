2d83139e1972b3ec35bdc17b1c985d9fd704d005
Opilane TTHK <opilane@tthk.ee>
2025-11-04 11:37:03 +0200

    Statistika

--- SQL/ADWfunktsioonid.sql
+++ SQL/ADWfunktsioonid.sql
@@ -2,3 +2,7 @@ SELECT TOP 5 ProductKey, SUM(SalesAmount) AS TotalSales
 FROM FactResellerSales
 GROUP BY ProductKey
 ORDER BY TotalSales DESC;
+
+SELECT AVG(SalesAmount) AS AvgSales,
+MIN(SalesAmount) AS MinSales,
+MAX(SalesAmount) AS MaxSales FROM FactResellerSales;
