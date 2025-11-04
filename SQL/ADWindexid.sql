9c0c3ce8ab900ca67c7b814d34bcc3e32546f86b
Opilane TTHK <opilane@tthk.ee>
2025-11-04 13:09:23 +0200

    Indeks tootele

--- SQL/ADWindexid.sql
+++ SQL/ADWindexid.sql
@@ -1,10 +1,2 @@
-CREATE NONCLUSTERED INDEX IX_FactInternetSales_Product
-ON FactInternetSales(ProductKey);
+CREATE INDEX idx_ProductKey ON FactResellerSales(ProductKey);
 
-
-CREATE UNIQUE INDEX IX_DimCustomer_Email
-ON DimCustomer(EmailAddress);
-
-
-CREATE NONCLUSTERED INDEX IX_FactInternetSales_OrderDate
-ON FactInternetSales(OrderDate);
