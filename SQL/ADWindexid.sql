88976093d394860df96076ffecf78531f7345df2
Nikita Gontsarov TARpv24 <78666039+Nikitagontsrovtkvgee@users.noreply.github.com>
2026-01-26 12:49:30 +0200

    Update ADWindexid.sql

--- SQL/ADWindexid.sql
+++ SQL/ADWindexid.sql
@@ -1,3 +1,5 @@
+-- Loon indeksi ProductKey veerule, et kiirendada päringuid
 CREATE INDEX idx_ProductKey ON FactResellerSales(ProductKey);
 
+-- Loon indeksi ResellerKey veerule paremaks otsinguks
 CREATE INDEX idx_ResellerKey ON FactResellerSales(ResellerKey);
