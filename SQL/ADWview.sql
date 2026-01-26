28703f4d5cc303a591647cc84a09ea1347912ee2
Nikita Gontsarov TARpv24 <78666039+Nikitagontsrovtkvgee@users.noreply.github.com>
2026-01-26 12:50:21 +0200

    Update ADWview.sql

--- SQL/ADWview.sql
+++ SQL/ADWview.sql
@@ -1,15 +1,20 @@
+-- Loon vaate, mis näitab TOP 5 edasimüüjat müügi alusel
 CREATE VIEW vw_TopResellers 
-AS SELECT TOP 5 ResellerKey, 
-SUM(SalesAmount) AS TotalSales 
+AS 
+SELECT TOP 5 ResellerKey, 
+       SUM(SalesAmount) AS TotalSales 
 FROM FactResellerSales 
 GROUP BY ResellerKey 
 ORDER BY TotalSales DESC;
 
+-- Kuvan vaate tulemused
 SELECT * FROM vw_TopResellers;
 
+-- Loon vaate, mis näitab müügi summat tootekategooriate kaupa
 CREATE VIEW vw_CategorySales
 AS
-SELECT pc.ProductCategoryKey, pc.EnglishProductCategoryName,
+SELECT pc.ProductCategoryKey, 
+       pc.EnglishProductCategoryName,
        SUM(f.SalesAmount) AS TotalSales
 FROM FactResellerSales f
 JOIN DimProduct p ON f.ProductKey = p.ProductKey
@@ -17,4 +22,5 @@ JOIN DimProductSubcategory sc ON p.ProductSubcategoryKey = sc.ProductSubcategory
 JOIN DimProductCategory pc ON sc.ProductCategoryKey = pc.ProductCategoryKey
 GROUP BY pc.ProductCategoryKey, pc.EnglishProductCategoryName;
 
+-- Kuvan vaate tulemused
 SELECT * FROM vw_CategorySales;
